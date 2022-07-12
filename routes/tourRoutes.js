bb87541138fd9eaf4fc594a1c3f88ebd97a515f5
JC <jasonqc9103@gmail.com>
2022-07-12 11:01:33 -0400

    add model tour data

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,21 +4,23 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  createTour,
-  checkID,
-  checkBody
+  createTour
+  // checkID,
+  // checkBody
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', checkID);
+// router.param('id', checkID);
 
 // create a checkBody middleware
 // check if body contains the name and price property
 // If not, send back 400 bad request
 // add it to the post handler stack
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+// router.route('/').get(getAllTours).post(checkBody,createTour);
+
+router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
--- server.js
+++ server.js
@@ -19,24 +19,21 @@ mongoose
   })
   .then(() => console.log('DB connection successfully!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
+// test db and insert data
+// const testTour = new Tour({
+//   name: 'The Forest Hiker',
+//   rating: 4.7,
+//   price: 497
+// });
 
-const Tour = mongoose.model('Tour', tourSchema);
-
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log('ERROR:👿', err);
+//   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
