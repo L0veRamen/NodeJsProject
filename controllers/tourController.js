546da368a8ca7fb2215a595d8825dcb47e01a76c
JC <jasonqc9103@gmail.com>
2022-07-12 15:05:05 -0400

    add filtering query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,10 +29,41 @@ const Tour = require('../models/tourModel');
 // };
 
 exports.getAllTours = async (req, res) => {
-  // console.log(req.requestTime);
   try {
-    const tours = await Tour.find();
+    // build query
+    // 1) filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // console.log(req.query, queryObj);
+
+    // 2) advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    //{difficulty: 'easy', duration:{$gte: 5}}
+    //{ difficulty: 'easy', duration: { gte: '5' } }
+    //gte, gt, lte, lt
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // const query = Tour.find(queryObj);
+
+    // console.log(req.query);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // execute query
+    const tours = await query;
+
+    // send response
     res.status(200).json({
       status: 'success',
       // requestAt: req.requestTime
