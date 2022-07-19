80d637b258b23dd7ffcd78dbdf44e82ba76ecc83
JC <jasonqc9103@gmail.com>
2022-07-19 14:00:06 -0400

    add error controller and error routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -16,10 +17,10 @@ app.use(express.json());
 //add static file
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('hello from middleware 🎃');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('hello from middleware 🎃');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -44,4 +45,32 @@ app.use((req, res, next) => {
 // 3) routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'FAIL',
+  //   message: `Can not find ${req.originalUrl} on this server! `
+  // });
+
+  // const err = new Error(`Can not find ${req.originalUrl} on this server! `);
+  // err.status = 'Fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can not find ${req.originalUrl} on this server! `, 404));
+});
+
+// app.use((err, req, res, next) => {
+//   console.log(err.stack);
+
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
+
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message
+//   });
+// });
+
+app.use(globalErrorHandler);
+
 module.exports = app;
