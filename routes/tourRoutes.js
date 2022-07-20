271756d27905da3b10a5f1dd9e5669d5f040b272
JC <jasonqc9103@gmail.com>
2022-07-20 15:10:41 -0400

    check login

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,6 +12,8 @@ const {
   // checkBody
 } = require('../controllers/tourController');
 
+const authController = require('../controllers/authController');
+
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
@@ -27,7 +29,7 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 // router.route('/').get(getAllTours).post(checkBody,createTour);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(authController.protect, getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
