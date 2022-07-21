ae2ea83779b6f75dd61ea0ea38db1677b8754cbf
JC <jasonqc9103@gmail.com>
2022-07-20 23:40:04 -0400

    finish sanitation

--- app.js
+++ app.js
@@ -1,84 +1,75 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// 1) global middleware
+// 1) GLOBAL middleware
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour! '
+  message: 'Too many requests from this IP, please try again in an hour!'
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
-//add static file
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log('hello from middleware 🎃');
-//   next();
-// });
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
   next();
 });
 
-//read all
-// app.get('/api/v1/tours', getAllTours);
-
-//read one
-// app.get('/api/v1/tours/:id', getTour);
-
-//Create
-// app.post('/api/v1/tours', createTour);
-
-//update
-// app.patch('/api/v1/tours/:id', updateTour);
-
-//delete
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-// 3) routes
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'FAIL',
-  //   message: `Can not find ${req.originalUrl} on this server! `
-  // });
-
-  // const err = new Error(`Can not find ${req.originalUrl} on this server! `);
-  // err.status = 'Fail';
-  // err.statusCode = 404;
-
-  next(new AppError(`Can not find ${req.originalUrl} on this server! `, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
-// app.use((err, req, res, next) => {
-//   console.log(err.stack);
-
-//   err.statusCode = err.statusCode || 500;
-//   err.status = err.status || 'error';
-
-//   res.status(err.statusCode).json({
-//     status: err.status,
-//     message: err.message
-//   });
-// });
 
 app.use(globalErrorHandler);
 
