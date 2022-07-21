1e25961d445cbe1deb16f834e6c6e732ec66c218
JC <jasonqc9103@gmail.com>
2022-07-20 20:38:58 -0400

    set user inactive

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteUser
 } = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -16,14 +17,14 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-// router.patch(
-//   '/updateMyPassword',
-//   authController.protect,
-//   authController.updatePassword
-// );
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
 
-// router.patch('/updateMe', authController.protect, userController.updateMe);
-// router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
